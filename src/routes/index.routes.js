2efbebbb82dd8e61a124f61e1b6c2fd97cf7b11d
unknown <mathiasimagine@gmail.com>
2022-07-04 21:47:21 -0300

    config server

---
+++ src/routes/index.routes.js
@@ -0,0 +1,13 @@
+import {Router} from "express"
+const router = Router()
+
+router.get("/", (req,res) => {
+    res.send("Holaaa")
+})
+
+router.get("/about", (req,res) => {
+    res.send("<h1>About</h1>")
+})
+
+
+export default router;
